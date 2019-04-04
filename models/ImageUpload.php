665a635c58395a33da77a8fb2b33758bd158da06
kanvik175 <r.sarzhin@gmail.com>
2019-04-04 23:05:16 +0300

    создание класса загрузки картинок https://www.youtube.com/watch?v=2NOHrukCHBU&list=PLa9lO_Eq-vZWucOH1auan_m-POa3n7fV6&index=8

---
+++ models/ImageUpload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rus
+ * Date: 04.04.19
+ * Time: 22:55
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+class ImageUpload extends Model {
+
+    public $image;
+
+}
