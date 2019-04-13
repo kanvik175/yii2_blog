0eb6f0a962563a959ce9f41b22a7a6be45682585
kanvik175 <r.sarzhin@gmail.com>
2019-04-13 16:41:12 +0300

    доделал загрузку картинок https://www.youtube.com/watch?v=2NOHrukCHBU

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -8,10 +8,62 @@
 
 namespace app\models;
 
+use Yii;
 use yii\base\Model;
+use yii\web\UploadedFile;
 
 class ImageUpload extends Model {
 
     public $image;
+
+    public function rules()
+    {
+        return [
+          [['image'], 'required'],
+          [['image'], 'file', 'extensions' => 'jpg,png']
+        ];
+    }
+
+    public function uploadFile(UploadedFile $file, $currentImage)
+    {
+
+        $this->image = $file;
+
+        if ($this->validate())
+        {
+            $this->delCurImage($currentImage);
+
+            return $this->saveImage();
+        }
+
+        return false;
+    }
+
+    private function getFolder($file = '')
+    {
+        return Yii::getAlias('@web') . 'uploads/' . $file;
+    }
+
+    private function genFileName()
+    {
+        return strtolower(md5(uniqid($this->image->baseName)) . "." . $this->image->extension);
+    }
+
+    public function delCurImage($currentImage)
+    {
+        if (isset($currentImage)) {
+            $old_file_path = $this->getFolder($currentImage);
+            file_exists($old_file_path) ? unlink($old_file_path) : null;
+        }
+    }
+
+    public function saveImage()
+    {
+        $filename = $this->genFileName();
+        $this->image->saveAs($this->getFolder($filename));
+
+        return $filename;
+    }
+
 
 }
